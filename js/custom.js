d48dc821fd64c3974ab830dc228dda3fe30b3d64
Ivan Lisouski <ivan_lisouski@epam.com>
2019-12-17 01:49:48 +0300

    HTML hometask

--- js/custom.js
+++ js/custom.js
@@ -1,38 +1,58 @@
 (function() {
-  var burger = document.querySelector(".burger");
-  var menu = document.querySelector("#" + burger.dataset.target);
-  burger.addEventListener("click", function() {
-    burger.classList.toggle("is-active");
-    menu.classList.toggle("is-active");
+  var burger = document.querySelector('.burger');
+  var menu = document.querySelector('#' + burger.dataset.target);
+  burger.addEventListener('click', function() {
+    burger.classList.toggle('is-active');
+    menu.classList.toggle('is-active');
   });
 })();
 
-document.querySelectorAll("#nav li").forEach(function(navEl) {
+document.querySelectorAll('#nav li').forEach(function(navEl) {
   navEl.onclick = function() {
     toggleTab(this.id, this.dataset.target);
   };
 });
 
 function toggleTab(selectedNav, targetId) {
-  var navEls = document.querySelectorAll("#nav li");
+  var navEls = document.querySelectorAll('#nav li');
 
   navEls.forEach(function(navEl) {
     if (navEl.id == selectedNav) {
-      navEl.classList.add("is-active");
+      navEl.classList.add('is-active');
     } else {
-      if (navEl.classList.contains("is-active")) {
-        navEl.classList.remove("is-active");
+      if (navEl.classList.contains('is-active')) {
+        navEl.classList.remove('is-active');
       }
     }
   });
 
-  var tabs = document.querySelectorAll(".tab-pane");
+  var tabs = document.querySelectorAll('.tab-pane');
 
   tabs.forEach(function(tab) {
     if (tab.id == targetId) {
-      tab.style.display = "block";
+      tab.style.display = 'block';
     } else {
-      tab.style.display = "none";
+      tab.style.display = 'none';
     }
   });
 }
+
+document.querySelector('.toggle-skip-nav').onclick = function() {
+  this.parentNode.classList.add('is-active');
+  document.querySelector('.skip-nav-dropdown a').focus();
+};
+
+document.querySelector('.skip-nav-dropdown').addEventListener('focusout', function(event) {
+  document.querySelector('.skip-nav').classList.remove('is-active');
+});
+
+document.querySelector('.skip-nav-dropdown').addEventListener('keydown', function(event) {
+  switch (event.keyCode) {
+    case 40:
+      console.log('down');
+      break;
+    case 38:
+      console.log('up');
+      break;
+  }
+});
